dced9dd919a0c52e721c05af3fe265b50b89aea1
Mike Risse <rissem@gmail.com>
2014-06-08 00:55:40 -0400

    integrate with docker-py

--- dockernotebook/index.py
+++ dockernotebook/index.py
@@ -1,9 +1,26 @@
 from flask import Flask
+import docker
+
+client = docker.Client(base_url='unix:///var/run/docker.sock',
+                  version='1.9',
+                  timeout=10)
+
+#print client.info()
+container = client.create_container("unfairbanks/docker-ipython-notebook", ports=[8080])
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/create")
+def create():
+    container = client.create_container("unfairbanks/docker-ipython-notebook", ports=[8080])
+    containerId = container['Id']
+    client.start(containerId, publish_all_ports=True)
+    port = client.port(containerId, 8080)[0]["HostPort"]
+    return port
 
 if __name__ == "__main__":
     app.debug = True
