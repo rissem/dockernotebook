3fc695b26ea808241206348dd6f81d41ecbd9e3a
Mike Risse <rissem@gmail.com>
2014-06-08 17:03:08 -0400

    nicer URL page

--- dockernotebook/index.py
+++ dockernotebook/index.py
@@ -3,6 +3,35 @@ import docker
 import requests
 import time
 
+
+containerURLPagePart1 = """
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Docker Notebook</title>
+
+    <!-- Bootstrap -->
+    <link href="/static/css/bootstrap.css" rel="stylesheet">
+    <link href="/static/css/dockerNotebook.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body><h1>
+"""
+
+containerURLPagePart2 = """
+  </h1></body>
+</html
+"""
+
 client = docker.Client(base_url='unix:///var/run/docker.sock',
                   version='1.9',
                   timeout=10)
@@ -54,7 +83,7 @@ def create():
                 serverStarted = True
         except Exception as e:
             print("CONNECTION ERROR", e)
-    return ("<a href='http://dockernotebook.com:" + port + "/'>Your Notebook</a>")
+    return (containerURLPagePart1 + "<a href='http://dockernotebook.com:" + port + "/'>Your Notebook</a>" + containerURLPagePart2)
 
 if __name__ == "__main__":
     app.debug = True
